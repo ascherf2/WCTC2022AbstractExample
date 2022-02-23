15b7330b68161e0d2af97852111f7ea29a02f3e8
Allie Scherf <ascherf2@my.wctc.edu>
2022-02-23 16:00:34 -0600

    Added cow class to program.cs

---
+++ Models/Cow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Cow : Animal
+    {
+
+
+        public Cow(string name)
+        {
+            Name = name;
+        }
+
+        public Cow()
+        {
+            Name = "George";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says moooooooo");
+
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine($"ZZZZzzzzz");
+        }
+    }
+}
+    
